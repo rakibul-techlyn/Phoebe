a50acae1a3d9aa7586d9aee61f7c865addeb8625
rakibul-techlyn <rakibul.techlyn@gmail.com>
2024-09-18 17:29:02 +0600

    tab

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let tabs = document.querySelectorAll(".seller_tab .tabs .tab");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+  });
+});

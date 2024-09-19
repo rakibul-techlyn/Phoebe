a339cbb9b9304a549e1feca91f8de2032e509af9
rakibul-techlyn <rakibul.techlyn@gmail.com>
2024-09-19 10:03:42 +0600

    product

--- js/script.js
+++ js/script.js
@@ -8,3 +8,89 @@ tabs.forEach((tab, index) => {
     tab.classList.add("active");
   });
 });
+
+var swiper = new Swiper(".bestSellerTab", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    340: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    550: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    840: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 7,
+      spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".products-tab", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: "",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".seller_tab .products-tab .swiper-pagination",
+    type: "progressbar",
+  },
+  breakpoints: {
+    340: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    360: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    840: {
+      slidesPerView: 2.4,
+      spaceBetween: 20,
+    },
+    950: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    1300: {
+      slidesPerView: 4.5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
